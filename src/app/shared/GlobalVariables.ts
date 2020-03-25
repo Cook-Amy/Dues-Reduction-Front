9af5c1837a332fc868eb928960059528882a04ae
Cook-Amy <coo17045@byui.edu>
2020-03-25 09:22:59 -0400

    fixes during build

--- src/app/shared/GlobalVariables.ts
+++ src/app/shared/GlobalVariables.ts
@@ -1,7 +1,6 @@
 export class GlobalVariables {
-    public static serverUrl = 'http://localhost:4000/';
-  // public static serverUrl: string = 'http://drbackend-env-1.eba-jturgbjy.us-east-2.elasticbeanstalk.com/';
-  // public static serverUrl: string = 'http://ec2-3-135-126-20.us-east-2.compute.amazonaws.com/';
+    // public static serverUrl: string = 'http://localhost:4000/';
+  public static serverUrl: string = 'http://drbackend-env-1.eba-jturgbjy.us-east-2.elasticbeanstalk.com/';
 
 
 
