9d86cd512f6b2f5f3203239f16faf737ae548ecd
Cook-Amy <coo17045@byui.edu>
2020-11-30 16:32:48 -0500

    removed dtTrigger

--- src/app/shared/GlobalVariables.ts
+++ src/app/shared/GlobalVariables.ts
@@ -1,6 +1,6 @@
 export class GlobalVariables {
-    public static serverUrl: string = 'http://localhost:4000/';
-  // public static serverUrl: string = 'http://drbackend-env-1.eba-jturgbjy.us-east-2.elasticbeanstalk.com/';
+  //  public static serverUrl: string = 'http://localhost:4000/';
+   public static serverUrl: string = 'http://drbackend-env-1.eba-jturgbjy.us-east-2.elasticbeanstalk.com/';
 
 
 
